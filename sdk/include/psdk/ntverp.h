4cda75d29dabc58162740825eddd9cca7340dc2c
Michaelsoft1853 <oyunpc112@gmail.com>
2024-12-28 00:15:40 +0300

    Update ntverp.h

--- sdk/include/psdk/ntverp.h
+++ sdk/include/psdk/ntverp.h
@@ -16,16 +16,16 @@
 //
 // Windows NT Build 3790.3959
 //
-#define VER_PRODUCTBUILD                    3790
+#define VER_PRODUCTBUILD                    6666
 #define VER_PRODUCTBUILD_QFE                3959
 
 //
 // Windows NT Version 5.2
 //
 #define VER_PRODUCTMAJORVERSION             5
-#define VER_PRODUCTMINORVERSION             2
-#define VER_PRODUCTVERSION_W                (0x0502)
-#define VER_PRODUCTVERSION_DW               (0x05020000 | VER_PRODUCTBUILD)
+#define VER_PRODUCTMINORVERSION             3
+#define VER_PRODUCTVERSION_W                (0x0503)
+#define VER_PRODUCTVERSION_DW               (0x05030000 | VER_PRODUCTBUILD)
 
 //
 // Not a beta
